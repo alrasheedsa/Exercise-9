dc5fb65a24a5ee6ad3629ced20a3d3fe1bbe51f8
Mohammed Alrasheed <m.alrasheedsaa@gmail.com>
2026-04-15 14:52:16 +0300

    Update Product.java

--- Exercise9/Product.java
+++ Exercise9/Product.java
@@ -4,9 +4,7 @@ public abstract class Product {
     private String name;
     private double price;
 
-    public Product(){
-
-    }
+    public Product(){}
 
     public Product(String name, double price) {
         this.name = name;
@@ -31,5 +29,12 @@ public abstract class Product {
 
     // Set getDiscount as abstract to use it athoer classes
     public abstract double getDiscount();
+
+    // Set getBuy1and1Free as abstract to use it another classes
+    public abstract String getBuy1and1Free();
+
+    //another method 
+    public abstract double getPoints();
+
 
 }
